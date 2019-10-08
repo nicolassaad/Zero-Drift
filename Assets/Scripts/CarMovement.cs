7203838457abe4011811f9b57c8d30b9f48c9acc
Trevor Hedstrom <Shmaug@users.noreply.github.com>
2019-10-07 19:55:06 -0700

    new fuckin everything

--- Assets/Scripts/CarMovement.cs
+++ Assets/Scripts/CarMovement.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Text;
 
 // TODO Add a handbrake: Applying handbrake should lessen carGrip and increase turnspeed as long as the handbrake is being held.
 //      Or maybe you can even momentarily change the ForceMode to VelocityChange while the brake is being held. 
@@ -6,14 +7,39 @@
 //      Ground box collision can be a trigger and make a boolean that can be checked in FixedUpdate(). If carOnGround = true
 //      then run all the code in FixedUpdate(). Otherwise return so nothing happens.
 
+#if UNITY_EDITOR
+using UnityEditor;
+[CustomEditor(typeof(CarMovement))]
+public class CarMovementEditor : Editor {
+    public override void OnInspectorGUI() {
+        base.OnInspectorGUI();
+
+        CarMovement car = target as CarMovement;
+
+        EditorGUI.BeginChangeCheck();
+        AnimationCurve pitch = EditorGUILayout.CurveField("Engine Pitch", car.EnginePitch, Color.green, new Rect(0f, 0f, 1f, 10f));
+        AnimationCurve lateralGrip = EditorGUILayout.CurveField("Laterial Grip", car.LateralGrip, Color.green, new Rect(0f, 0f, 1f, 1f));
+        AnimationCurve spinSlip = EditorGUILayout.CurveField("Spin Grip", car.SpinGrip, Color.green, new Rect(0f, 0f, 1f, 1f));
+        if (EditorGUI.EndChangeCheck()) {
+            Undo.RecordObject(car, "Edit AnimationCurve");
+            car.EnginePitch = pitch;
+            car.LateralGrip = lateralGrip;
+            car.SpinGrip = spinSlip;
+        }
+    }
+}
+#endif
+
 public class CarMovement : MonoBehaviour {
     public float SteerThrow = 45f;
-    public float EnginePower = 1650f;
-    public float TireGrip = 140f;
-    public float RedlineRPM = 8000f;
+    public float EngineTorque = 10f;
+    public float LateralGripMultiplier = 140f;
+    public float SlipForceMultiplier = 140f;
     public float SteerSpeed = 45f; // deg/sec
-    public float Drag = 10f;
-    public float MaxSpeed = 25f;
+    public float MaxSpeed = 50f; // m/s
+    public float MaxSlip = 1f;
+
+    public UnityEngine.UI.Text SpeedText;
 
     public Transform CoM;
     public Transform[] Wheels;
@@ -21,164 +47,134 @@ public class CarMovement : MonoBehaviour {
     public Transform[] TireContact;
     public int[] PowerContact;
 
-    public ParticleSystem leftParticle;
-    public ParticleSystem rightParticle;
+    // gonna show these with a custom editor (see above)
+    [HideInInspector]
+    public AnimationCurve EnginePitch;
+    [HideInInspector]
+    public AnimationCurve LateralGrip;
+    [HideInInspector]
+    public AnimationCurve SpinGrip;
 
     private float carSpeed;
-    private Vector3[] GroundNormal;
-    private Vector3[] TireForces;
+    private Vector3[] groundNormal; // the normal of the ground under each tire
 
-    public float EnginePitchMin = 1f;
-    public float EnginePitchMax = 2f;
+    private ParticleSystem[] tireSmoke;
 
     private Rigidbody rbody;
 
-    private AudioSource audioSrc;
+    private AudioSource engineAudio;
 
-    private float ThrottleInput;
-    private float SteerInput;
-    private float SteerRotation;
-    private float EngineRPM;
-    private bool HandBrake;
-    private float[] WheelSpin;
+    private float tireRadius;
+    private float throttleInput;
+    private float steerInput;
+    private float steerRotation;
+    private bool handBrake;
+    private float[] wheelSpeed;
+    private float[] tireRotation;
 
     void Start() {
         rbody = GetComponent<Rigidbody>();
         rbody.centerOfMass = CoM.localPosition;
-        audioSrc = GetComponent<AudioSource>();
-     
-        GroundNormal = new Vector3[TireContact.Length];
-        WheelSpin = new float[Wheels.Length];
-        TireForces = new Vector3[TireContact.Length];
+        engineAudio = GetComponent<AudioSource>();
+
+        groundNormal = new Vector3[TireContact.Length];
+        wheelSpeed = new float[Wheels.Length];
+        tireRotation = new float[Wheels.Length];
+
+        tireRadius = Wheels[0].GetComponent<SphereCollider>().radius;
+
+        tireSmoke = new ParticleSystem[TireContact.Length];
+        for (int i = 0; i < TireContact.Length; i++)
+            tireSmoke[i] = TireContact[i].GetComponent<ParticleSystem>();
     }
 
     private void FixedUpdate() {
-
-        // Increment Engine RPM
-        if (ThrottleInput != 0f)
-            EngineRPM += Mathf.Abs(ThrottleInput) * RedlineRPM * Time.fixedDeltaTime;
-        else
-            EngineRPM -= RedlineRPM * Time.fixedDeltaTime;
-        EngineRPM = Mathf.Clamp(EngineRPM, 0, RedlineRPM);
-
         // Rotate front tires for steering
-        if (SteerInput != 0f)
-            SteerRotation += SteerInput * SteerSpeed * Time.fixedDeltaTime;
+        float desiredSteer = steerInput * SteerThrow;
+        float steerStep = SteerSpeed * Time.fixedDeltaTime;
+        if (Mathf.Abs(desiredSteer - steerRotation) < steerStep)
+            steerRotation = desiredSteer; // if the amount remaining is smaller than the step size, then just snap to the desired angle (rather than overshooting).
         else
-            SteerRotation += Mathf.Min(Mathf.Abs(SteerRotation), SteerSpeed * Time.fixedDeltaTime) * -Mathf.Sign(SteerRotation);
-         SteerRotation = Mathf.Clamp(SteerRotation, -SteerThrow, SteerThrow);
-         Quaternion steer = Quaternion.Euler(0, SteerRotation, 0);
-
+            steerRotation += steerStep * Mathf.Sign(desiredSteer - steerRotation);
+        Quaternion steer = Quaternion.Euler(0, steerRotation, 0);
         for (int i = 0; i < Steering.Length; i++)
             Steering[i].localRotation = steer;
 
-        // Split engine power over all wheels that provide power
-        float engineForce = ThrottleInput * EnginePower / PowerContact.Length;
+        // increase the wheelSpeed of the tires that are under power
+        float desiredWheelSpeed = throttleInput * MaxSpeed * tireRadius;
         for (int i = 0; i < PowerContact.Length; i++) {
-            if (GroundNormal[PowerContact[i]].sqrMagnitude < .001f) continue;
-            Vector3 dir = Vector3.ProjectOnPlane(TireContact[PowerContact[i]].forward, GroundNormal[PowerContact[i]]);
-            rbody.AddForceAtPosition(dir * engineForce, TireContact[PowerContact[i]].position); // apply power from the contact patch from each wheel that receives engine power
+            float step = (Mathf.Abs(throttleInput) + 1f) * EngineTorque * Time.fixedDeltaTime;
+            if (Mathf.Abs(desiredWheelSpeed - wheelSpeed[PowerContact[i]]) < step)
+                wheelSpeed[PowerContact[i]] = desiredWheelSpeed;
+            else
+                wheelSpeed[PowerContact[i]] += step * Mathf.Sign(desiredWheelSpeed - wheelSpeed[PowerContact[i]]);
+
+            // hard limit for max speed: do not allow tires to spin faster than MaxSpeed
+            wheelSpeed[PowerContact[i]] = Mathf.Min(wheelSpeed[PowerContact[i]], MaxSpeed * tireRadius);
         }
 
-        // Grip force
+        // Tire physics
         for (int i = 0; i < TireContact.Length; i++) {
-            if (GroundNormal[i].sqrMagnitude < .001f) continue;
-            // compute velocity of car relative to the tire
-            Vector3 groundVel = Vector3.ProjectOnPlane(rbody.GetPointVelocity(TireContact[i].position), GroundNormal[i]);
+            if (groundNormal[i].sqrMagnitude < .01f) continue; // Don't do anything if the tire is not on the ground
+
+            // Compute velocity of car relative to the tire
+            Vector3 groundVel = Vector3.ProjectOnPlane(rbody.GetPointVelocity(TireContact[i].position), groundNormal[i]);
             Vector3 relVel = TireContact[i].InverseTransformVector(groundVel);
-            WheelSpin[i] += relVel.z / Time.fixedDeltaTime / .304f; // TODO: make .304 (the wheel radius) not hard-coded
 
-            relVel.y = relVel.z = 0;
+            // lateral grip: comes just from the lateral (right/left) velocity of the wheel
+            // Lateral grip goes exactly right or left of the wheel, thus keeping the car "on track"
+            float lateralForce = LateralGrip.Evaluate(Mathf.Abs(relVel.x) / MaxSpeed);
 
-            float a = .7f;
-            float b = 2f;
-            float k = .309f;
+            // slip comes from the difference in the linear speed of the bottom of the tire (from spinning), and the actual linear ground speed
+            float slip = (wheelSpeed[i] * tireRadius - relVel.z);
 
-            float x = a * (Mathf.Abs(relVel.x) - k);
-            float force = (a * (x - k) * Mathf.Exp(1 - a * (x - k)) + b) / (b + 1);
+            // spinForce scales with the ratio of wheelSpeed * radius / (fwdVel + 1)
+            float spinForce = Mathf.Sign(slip) * SpinGrip.Evaluate(Mathf.Abs(slip) / MaxSlip);
 
-            TireForces[i] = TireContact[i].right * -Mathf.Sign(relVel.x) * force * TireGrip;
+            rbody.AddForceAtPosition(TireContact[i].right * -Mathf.Sign(relVel.x) * lateralForce * LateralGripMultiplier, TireContact[i].position);
+            rbody.AddForceAtPosition(TireContact[i].forward * spinForce * SlipForceMultiplier, TireContact[i].position);
 
-            rbody.AddForceAtPosition(TireForces[i], TireContact[i].position); 
-            Wheels[i].localRotation = Quaternion.Euler(0, 0, WheelSpin[i] * Mathf.Deg2Rad);
+            wheelSpeed[i] += -spinForce * tireRadius * Time.fixedDeltaTime;
+            tireRotation[i] += wheelSpeed[i] * Time.fixedDeltaTime;
 
-            if (HandBrake)
-            {
-                // Stopping the wheelspin from increasing
-                WheelSpin[i] += 0f;
+            Wheels[i].localRotation = Quaternion.Euler(0, 0, tireRotation[i] * Mathf.Rad2Deg);
 
-                // Adding force with handbrake on
-                rbody.AddForceAtPosition(TireForces[i], TireContact[i].position);
-
-                // Turning tire smoke on
-                leftParticle.Play(); 
-                rightParticle.Play();
-            } else
-            {
-                //Turning tire smoke off
-                leftParticle.Stop();
-                rightParticle.Stop();
-            }
+            ParticleSystem.EmissionModule em = tireSmoke[i].emission;
+            em.enabled = Mathf.Abs(slip) > MaxSlip * .5f || Mathf.Abs(relVel.x) > MaxSpeed * .5f;
         }
-
-        // Drag
-        rbody.AddForce(-rbody.velocity * Vector3.Dot(rbody.velocity, rbody.velocity) * Drag);
 
         carSpeed = rbody.velocity.magnitude;
 
-        Debug.Log(Mathf.Round(carSpeed / 1.609f) + " MPH"); //Pretty sure were working with metric system here
+        SpeedText.text = (carSpeed * 2.23694f).ToString("0") + " mph"; // m/s -> mph
     }
 
     void Update() {
         // Handle input
-        SteerInput = Input.GetAxis("Horizontal");
-        ThrottleInput = Input.GetAxis("Vertical");
-        HandBrake = Input.GetKey("space");
-    }
+        steerInput = Input.GetAxis("Horizontal");
+        throttleInput = Input.GetAxis("Vertical");
+        handBrake = Input.GetKey("space");
+        engineAudio.pitch = EnginePitch.Evaluate(carSpeed / MaxSpeed);
 
-    private void LateUpdate() {
-
-        // Factored out EngineRPM Engine Pitch Min and Max to fake a better sounding noise
-        audioSrc.pitch = 0.30f + carSpeed * 0.025f;
-
-        if (carSpeed > 30)
-        {
-            audioSrc.pitch = 0.25f + carSpeed * 0.024f;
-        }
-
-        if (carSpeed > 40)
-        {
-            audioSrc.pitch = 0.20f + carSpeed * 0.023f;
-        }
-
-        if (carSpeed > 49)
-        {
-            audioSrc.pitch = 0.15f + carSpeed * 0.022f;
-        }
-
-        // setting a max value for the pitch
-        if (audioSrc.pitch > 2.5)
-        {
-            audioSrc.pitch = 2.5f;
-        }
+        for (int i = 0; i < Wheels.Length; i++)
+            Debug.DrawLine(Wheels[i].position, Wheels[i].position + groundNormal[i]);
     }
 
     private void OnCollisionEnter(Collision collision) {
         for (int i = 0; i < collision.contacts.Length; i++)
             for (int j = 0; j < Wheels.Length; j++)
                 if (collision.contacts[i].thisCollider.transform == Wheels[j])
-                    GroundNormal[j] = collision.contacts[i].normal;
+                    groundNormal[j] = collision.contacts[i].normal;
     }
     private void OnCollisionStay(Collision collision) {
         for (int i = 0; i < collision.contacts.Length; i++)
             for (int j = 0; j < Wheels.Length; j++)
                 if (collision.contacts[i].thisCollider.transform == Wheels[j])
-                    GroundNormal[j] = collision.contacts[i].normal;
+                    groundNormal[j] = collision.contacts[i].normal;
     }
     private void OnCollisionExit(Collision collision) {
         for (int i = 0; i < collision.contacts.Length; i++)
             for (int j = 0; j < Wheels.Length; j++)
                 if (collision.contacts[i].thisCollider.transform == Wheels[j])
-                    GroundNormal[j] = Vector3.zero;
+                    groundNormal[j] = Vector3.zero;
     }
 }
